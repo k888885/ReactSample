ce51df5f773beb67cea426ce7dfab0d14906b580
k888885 <55884311+k888885@users.noreply.github.com>
2022-04-20 14:03:53 +0200

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"625ff5dc56d737002476d0de",
+				"625ff5dc56d737002476d0e1"
+			]
+		},
+		"625ff5dc56d737002476d0de": {
+			"id": "625ff5dc56d737002476d0de",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"625ff5dc56d737002476d0e1": {
+			"id": "625ff5dc56d737002476d0e1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
